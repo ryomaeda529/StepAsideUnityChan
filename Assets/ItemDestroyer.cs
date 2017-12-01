18df650f4ce7ee774696360d96a373071cd34e5d
ryomaeda529 <everyone.529.brother@docomo.ne.jp>
2017-12-02 02:05:16 +0900

    不要なアイテムの破棄
    ユニティちゃんが通り過ぎて画面外に出たアイテムを直ちに破棄するようにItenDestroyerを追加しました。

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+    private GameObject unitychan;
+    private float difference;
+
+    // Use this for initialization
+    void Start(){
+        this.unitychan = GameObject.Find("unitychan");
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag"){
+            Destroy(other.gameObject);
+
+        }
+    }
+
+}
