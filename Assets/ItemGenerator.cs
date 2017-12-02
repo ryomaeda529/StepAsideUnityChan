bf5a1b8f0d3bd185f9e3c97100343d61c49f64a2
ryomaeda529 <everyone.529.brother@docomo.ne.jp>
2017-12-02 16:28:04 +0900

    アイテムを動的に生成
    ユニティちゃんの位置に応じてアイテムが生成されるようにItemGeneratorを修正しました。

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemGenerator : MonoBehaviour {
+public class ItemGenerator : MonoBehaviour
+{
 
     public GameObject carPrefab;
     public GameObject coinPrefab;
@@ -12,41 +13,48 @@ public class ItemGenerator : MonoBehaviour {
     private int goalPos = 120;
     private float posRange = 3.4f;
 
-	// Use this for initialization
-	void Start () {
-        for (int i = startPos; i < goalPos; i += 15) {
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        if (unitychan.transform.position.z > startPos)
+        {
             int num = Random.Range(0, 10);
             if (num <= 1)
             {
                 for (float j = -1; j <= 1; j += 0.4f)
                 {
                     GameObject cone = Instantiate(conePrefab) as GameObject;
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, unitychan.transform.position.z + 40);
                 }
-            }else {
-                for (int j = -1; j < 2; j++) {
+            }
+            else
+            {
+                for (int j = -1; j < 2; j++)
+                {
                     int item = Random.Range(1, 11);
                     int offsetZ = Random.Range(-5, 6);
-
                     if (1 <= item && item <= 6)
                     {
                         GameObject coin = Instantiate(coinPrefab) as GameObject;
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, unitychan.transform.position.z + 40 + offsetZ);
                     }
-                    else if (7 <= item && item <= 9) {
+                    else if (7 <= item && item <= 9)
+                    {
                         GameObject car = Instantiate(carPrefab) as GameObject;
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, unitychan.transform.position.z + 40 + offsetZ);
                     }
-
                 }
-
             }
+            startPos += 15;
         }
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
